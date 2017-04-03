9ba22fb74fa4471227fa75c6c14ee6eeb1462d0e
JisongLiu <jl4819@columbia.edu>
2017-04-02 22:50:42 -0400

    Update register.js

--- register.js
+++ register.js
@@ -1,3 +1,5 @@
+var AWS = require('aws-sdk');
+var sns = new AWS.SNS({ region: config.AWS_REGION});
 console.log('Loading function');
       var doc = require('dynamodb-doc');
       var db = new doc.DynamoDB();
@@ -23,14 +25,27 @@ var item = {
 var flag=false;
  db.getItem()
  db.putItem(params,function(err,data){
-     if (err) {console.log("fail");console.log(err);
-         callback(null,flag);
+     if (err) {
+           console.log("fail");console.log(err);
      }
      else{ console.log("success");
          console.log(data);
          flag=true;
-         callback(null,flag);
      }
+       var params = {
+        Protocol: 'email',
+        TopicArn: arn:aws:sns:us-east-1:106682779689:MyTopics,   // not sure, add a name or url?
+        Endpoint: email
+      };
+       sns.subscribe(params, function(err, data) {
+        if (err) {
+          console.log(err, err.stack); // an error occurred
+        } else {
+          console.log('Working signup');
+          console.log(data);           // successful response
+        }   
+      }
+      callback(null,flag);
  });
  
 };
